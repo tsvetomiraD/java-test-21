76f0d5fd47d79f9961afab75d879e4afb2726e8a
tsvetomiraD <95402102+tsvetomiraD@users.noreply.github.com>
2022-12-12 09:46:32 +0200

    Update Mapper.java

--- Mapper.java
+++ Mapper.java
@@ -5,21 +5,15 @@ import org.apache.ibatis.annotations.Update;
 import java.util.Set;
 
 public interface Mapper {
-    @Select("SELECT * FROM products WHERE siteID=#{id}")
-    public Product getProductBySiteID(String id);
-
-    @Select("SELECT price FROM products WHERE siteID=#{id}")
-    public String getProductPrice(String id);
-
     @Select("SELECT * FROM products")
     public Set<Product> getAllProducts();
 
-    @Insert("INSERT INTO products(name,price,siteID,description,photo,date) VALUES (#{name},#{price},#{siteID},#{description},#{photo},#{date})")
+    @Insert("INSERT INTO products(name,price,id,description,photo,date) VALUES (#{name},#{price},#{id},#{description},#{photo},#{date})")
     public int insertProduct(Product product);
 
-    @Update("UPDATE products SET name={name},price={price},siteID={siteID},description={description},photo={photo},date={date} WHERE siteID = #{siteID}")
+    @Update("UPDATE products SET name=#{name},price=#{price},description=#{description},photo=#{photo},date=#{date} WHERE id = #{id}")
     int updateProduct(Product post, String SIteID);
 
-    @Update("UPDATE products SET availability={availability} WHERE siteID = #{siteID}")
+    @Update("UPDATE products SET availability=#{availability} WHERE id = #{id}")
     int updateAvailability(String SiteID, int availability);
 }
